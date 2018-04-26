14e81e5ac42c2679ef0d15a0ea01867b2832afd1
c.mj2000@yahoo.com <c.mj2000@yahoo.com>
2018-04-26 23:15:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,13 +33,15 @@ function viewCart() {
   if(cart.length === 0){
     return"Your shopping cart is empty."
   }
+  else{
   for(var i =0; i<cart.length; i++){
     item = cart[i]["itemName"]
     price = cart[i]["itemPrice"]
     
    
      }
-     
+      if (cart.length === 1){
+    return str += item + " at "+ "$" + price + "."
      
      
   }
